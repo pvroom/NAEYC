658058bc8253505bda83fbecc438a8db43f3c7c3
Peter <pvroom@convergence-us.com>
2018-11-12 16:53:37 -0500

    Version 1.59

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -20,6 +20,10 @@ export class MapPage {
 	myMap4: any;
 	myMap5: any;
 	myMap6: any;
+	myMap7: any;
+	myMap8: any;
+	myMap9: any;
+	myMap10: any;
 
 	constructor(public navCtrl: NavController, 
 				public platform: Platform) {
@@ -122,6 +126,69 @@ export class MapPage {
 		this.myMap6.fitBounds(bounds);
 		this.myMap6.setMaxBounds(bounds);
 
+
+		this.myMap7 = L.map('mapLevel7', {
+			crs: L.CRS.Simple,
+			minZoom: -1,
+			maxZoom: 1,
+			zoomControl: true
+		});
+
+		var bounds = L.latLngBounds([0, 0], [1200, 1700]);    // Normally 1000, 1000; stretched to 2000,1000 for AACD 2017
+		var image = L.imageOverlay('assets/img/m.png', bounds, {
+			attribution: 'Convergence'
+		}).addTo(this.myMap7);
+
+		this.myMap7.fitBounds(bounds);
+		this.myMap7.setMaxBounds(bounds);
+
+
+		this.myMap8 = L.map('mapLevel8', {
+			crs: L.CRS.Simple,
+			minZoom: -1,
+			maxZoom: 1,
+			zoomControl: true
+		});
+
+		var bounds = L.latLngBounds([0, 0], [1200, 1700]);    // Normally 1000, 1000; stretched to 2000,1000 for AACD 2017
+		var image = L.imageOverlay('assets/img/m2.png', bounds, {
+			attribution: 'Convergence'
+		}).addTo(this.myMap8);
+
+		this.myMap8.fitBounds(bounds);
+		this.myMap8.setMaxBounds(bounds);
+
+
+		this.myMap9 = L.map('mapLevel9', {
+			crs: L.CRS.Simple,
+			minZoom: -1,
+			maxZoom: 1,
+			zoomControl: true
+		});
+
+		var bounds = L.latLngBounds([0, 0], [1200, 1700]);    // Normally 1000, 1000; stretched to 2000,1000 for AACD 2017
+		var image = L.imageOverlay('assets/img/m3.png', bounds, {
+			attribution: 'Convergence'
+		}).addTo(this.myMap9);
+
+		this.myMap9.fitBounds(bounds);
+		this.myMap9.setMaxBounds(bounds);
+
+
+		this.myMap10 = L.map('mapLevel10', {
+			crs: L.CRS.Simple,
+			minZoom: -1,
+			maxZoom: 1,
+			zoomControl: true
+		});
+
+		var bounds = L.latLngBounds([0, 0], [1200, 1700]);    // Normally 1000, 1000; stretched to 2000,1000 for AACD 2017
+		var image = L.imageOverlay('assets/img/m4.png', bounds, {
+			attribution: 'Convergence'
+		}).addTo(this.myMap10);
+
+		this.myMap10.fitBounds(bounds);
+		this.myMap10.setMaxBounds(bounds);
 
 
 
